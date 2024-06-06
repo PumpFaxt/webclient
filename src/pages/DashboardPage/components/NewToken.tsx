5d08b6a307afbc5a5759c8d345e15acbae3a618b
Riya Jain <riya1jain567@gmail.com>
2024-06-06 18:16:59 +0530

    Add preview for tokens on dashboard

--- src/pages/DashboardPage/components/NewToken.tsx
+++ src/pages/DashboardPage/components/NewToken.tsx
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Coin } from "../../../types";
+import CoinCard from "../../../common/CoinCard";
+import { useAccount } from "wagmi";
 
 export default function NewToken() {
+  const { address } = useAccount();
+  const [imagePreview, setImagePreview] = useState<string>("");
+  const [token, setToken] = useState<any>({
+    name: "",
+    image_uri: imagePreview,
+    symbol: "",
+    creator: address,
+    description: "",
+  });
+
+  const handleChange = (e: { target: { name: string; value: string } }) => {
+    const { name, value } = e.target;
+    setToken((prevToken: any) => ({
+      ...prevToken,
+      [name]: value,
+    }));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      const imageUrl = URL.createObjectURL(file);
+      setImagePreview(imageUrl);
+      setToken((prevToken: any) => ({
+        ...prevToken,
+        image_uri: imageUrl,
+      }));
+    }
+  };
+
   return (
     <div className="p-5 flex flex-col gap-y-5">
       <div className="flex gap-x-5">
@@ -9,6 +43,8 @@ export default function NewToken() {
           name="name"
           placeholder="Name of token (eg: Frax Doge)"
           className="input-retro flex-1"
+          value={token.name}
+          onChange={handleChange}
         />
         <input
           type="text"
@@ -17,6 +53,8 @@ export default function NewToken() {
           minLength={2}
           maxLength={6}
           className="input-retro flex-1"
+          value={token.symbol}
+          onChange={handleChange}
         />
         <input
           type="number"
@@ -27,17 +65,29 @@ export default function NewToken() {
           className="input-retro flex-1"
         />
       </div>
+      <textarea
+        name="description"
+        placeholder="Token description"
+        className="input-retro flex-1"
+        value={token.description}
+        onChange={handleChange}
+      />
 
       <input
-        type="number"
+        type="file"
+        accept="image/*"
         name="image_url"
         placeholder="Image"
         className="input-retro"
+        onChange={handleImageChange}
       />
 
       <button className="btn-retro self-center px-8 py-1 -mb-2">
         Create Token
       </button>
+      <div className="bg-background w-max p-2">
+        <CoinCard coin={token} className="max-h-[20vh] max-w-[30vw]" />
+      </div>
     </div>
   );
 }
--- src/pages/ShowcasePage/components/Showcase.tsx
+++ src/pages/ShowcasePage/components/Showcase.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import useApiResponse from "../../../hooks/useApiResponse";
 import api from "../../../utils/api";
-import { Coin } from "../../../types";
 import { twMerge } from "tailwind-merge";
-import { getImageDominantRgb, getLuminicanceFromRgb } from "../../../utils";
-import { Link } from "react-router-dom";
+import CoinCard from "../../../common/CoinCard";
 
 export default function Showcase() {
   const coins = useApiResponse(api.dummy.coins);
@@ -65,71 +63,5 @@ export default function Showcase() {
         </div>
       )}
     </section>
-  );
-}
-
-function CoinCard(props: { coin: Coin; className?: string }) {
-  const data = props.coin;
-
-  const [color, setColor] = useState("rgb(255, 255, 255)");
-
-  async function loadData() {
-    const rgbClamped = await getImageDominantRgb(data.image_uri);
-    var rgb = [rgbClamped[0], rgbClamped[1], rgbClamped[2]];
-    while (getLuminicanceFromRgb(rgb) < 128) {
-      rgb[0] += 1;
-      rgb[1] += 1;
-      rgb[2] += 1;
-    }
-    const clr = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-
-    setColor(clr);
-  }
-
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  return (
-    <Link
-      to={`/T/${data.signature}`}
-      className={twMerge(
-        props.className,
-        "flex border border-front/20 rounded-lg select-none relative overflow-hidden group"
-      )}
-    >
-      <img
-        src="/images/rainbow-bg.gif"
-        alt="rainbouu"
-        className="absolute-cover opacity-0 group-hover:opacity-100 group-hover:duration-[100000ms] ease-out duration-[3000ms] -z-1"
-      />
-
-      <img
-        src={data.image_uri}
-        alt={data.name}
-        className="w-1/3 m-3 object-contain"
-      />
-      <div className="flex flex-col m-3 gap-y-1 group-hover:mix-blend-difference">
-        <h3
-          className="font-semibold tracking-wide text-lg"
-          style={{ color: color }}
-        >
-          {data.name
-            .replaceAll(/solana/gi, "Fraxtal")
-            .replaceAll(/sol/gi, "frax")}
-        </h3>
-        <h4 className="text-sm font-semibold text-front/70">
-          TICKER : {data.symbol}
-        </h4>
-
-        <h5 className="text-pink-400 truncate max-w-[16ch] text-xs my-[1px]">
-          Created by {data.creator}
-        </h5>
-
-        <p className="mt-1 text-sm text-front/90 font-light line-clamp-4">
-          {data.description.replaceAll(/sol/gi, "usd")}
-        </p>
-      </div>
-    </Link>
   );
 }
