1ab9a227bda06d9fda0ab61fe26c52258a49409f
Spandan Barve <spandan567@gmail.com>
2024-06-13 21:30:54 +0530

    remove unncecesary code

--- src/pages/HuddleVoicePage/components/Room.tsx
+++ src/pages/HuddleVoicePage/components/Room.tsx
@@ -27,19 +27,14 @@ export default function Room(props: RoomProps) {
 
   if (!address) return <></>;
 
-  const [requestedPeerId, setRequestedPeerId] = useState("");
   const avatarUrl = "https://wojak-studio.com/res/bases/happy_smug.png";
   const displayName = address;
-  // const isChatOpen = useStore((state) => state.isChatOpen);
-  const { updateMetadata, metadata, peerId, role } = useLocalPeer<{
+
+  const { updateMetadata, metadata } = useLocalPeer<{
     displayName: string;
     avatarUrl: string;
     isHandRaised: boolean;
   }>();
-  const { peerIds } = usePeerIds();
-
-  const { huddleClient } = useHuddle01();
-
   useEffect(() => {
     if (state === "idle") {
       props.leaveCall();
@@ -53,50 +48,10 @@ export default function Room(props: RoomProps) {
     }
   }, []);
 
-  useDataMessage({
-    onMessage(payload, from, label) {
-      if (label === "requestToSpeak") {
-        // setShowAcceptRequest(true);
-        setRequestedPeerId(from);
-        // addRequestedPeers(from);
-        setTimeout(() => {
-          // setShowAcceptRequest(false);
-        }, 5000);
-      }
-
-      if (label === "chat" && from !== peerId) {
-        const messagePayload = JSON.parse(payload);
-        const newChatMessage = {
-          name: messagePayload.name,
-          text: messagePayload.message,
-          is_user: false,
-        };
-        // addChatMessage(newChatMessage);
-      }
-    },
-  });
-
-  // useEffect(() => {
-  // if (!requestedPeers.includes(requestedPeerId)) {
-  // setShowAcceptRequest(false);
-  // }
-  // }, [requestedPeers]);
-
   return (
     <section className="bg-audio flex h-screen items-center justify-center w-full relative  text-slate-100">
       <Grid />
-
-      {/* <div className="flex items-center justify-center w-full">
-        <GridLayout />
-        <Sidebar />
-        <div className="absolute right-4 bottom-20">
-          {showAcceptRequest && <AcceptRequest peerId={requestedPeerId} />}
-        </div>
-      </div> */}
       <Options />
-      {/* {isChatOpen && <Chat />} */}
-      {/* <BottomBar /> */}
-      {/* <Prompts /> */}
     </section>
   );
 }
