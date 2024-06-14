c15cf4fb10373188afd36009ad4253a0eee4e244
Spandan Barve <spandan567@gmail.com>
2024-06-14 16:51:31 +0530

    Add api endpoint get token by address

--- src/utils/api/tokens.ts
+++ src/utils/api/tokens.ts
@@ -17,6 +17,13 @@ const token = {
     const data = response.data;
     return data;
   },
+
+  async getByAddress(address: string) {
+    const response = await client.get<{ token: Token }>(`/tokens/${address}`);
+
+    const data = response.data;
+    return data.token;
+  },
 };
 
 export default token;
