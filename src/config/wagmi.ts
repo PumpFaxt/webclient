fa054ad936511af4d4a6193bfd2385f4d2d506a9
Spandan Barve <spandan567@gmail.com>
2024-07-01 15:31:14 +0530

    add mainnet CA

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -4,23 +4,23 @@ import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { publicProvider } from "wagmi/providers/public";
 
-const fraxtalTestnet: Chain = {
+const fraxtal: Chain = {
   id: 2522,
-  name: "Fraxtal Testnet L2",
+  name: "Fraxtal mainnet",
   network: "Fraxtal",
   nativeCurrency: { symbol: "frxETH", name: "Frax Eth", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://rpc.testnet.frax.com"] },
-    public: { http: ["https://rpc.testnet.frax.com"] },
+    default: { http: ["https://rpc.frax.com"] },
+    public: { http: ["https://rpc.frax.com"] },
   },
   testnet: true,
   blockExplorers: {
-    default: { name: "fraxscan", url: "https://holesky.fraxscan.com" },
+    default: { name: "fraxscan", url: "https://fraxscan.com" },
   },
 };
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [fraxtalTestnet],
+  [fraxtal],
   [publicProvider()]
 );
 
@@ -34,13 +34,13 @@ const wagmiConfig = createConfig({
         projectId: "756f8ad5a4c44ce4fbd9897445a10187",
         qrModalOptions: { themeMode: "dark" },
         metadata: {
-          name: "Mars App Starter",
-          description: "Test kit of mars app",
+          name: "PumpFaxt",
+          description: "memecoin trading platform",
           icons: ["/favicon.ico"],
           url: window.location.hostname,
         },
       },
-      chains: [fraxtalTestnet],
+      chains: [fraxtal],
     }),
   ],
   publicClient,
