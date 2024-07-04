cebd0930446f72609b5df05f80b1934ed0b13f03
Spandan Barve <spandan567@gmail.com>
2024-07-05 00:20:52 +0530

    bugfix

--- src/common/UsernameWrapper.tsx
+++ src/common/UsernameWrapper.tsx
@@ -5,7 +5,7 @@ import contractDefinitions from "../contracts";
 
 export default function UsernameWrapper(props: { children: React.ReactNode }) {
   if (typeof props.children != "string" || !isAddress(props.children))
-    return props.children;
+    return <>{props.children}</>;
 
   const address = props.children;
 
