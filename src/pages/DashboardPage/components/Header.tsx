f4ecac431ba1cff8a606b0c7df07d535f69a599d
Riya Jain <riya1jain567@gmail.com>
2024-07-05 16:07:18 +0530

    update header

--- src/pages/DashboardPage/components/Header.tsx
+++ src/pages/DashboardPage/components/Header.tsx
@@ -32,75 +32,55 @@ export default function Header() {
         <div className="bg-tertiary p-1 border-2 border-front">
           <div className="bg-tertiary p-3 border-2 border-front">
             <div className="flex widescreen:justify-center widescreen:items-center gap-x-4 mobile:flex-col">
-              {username?.data?.toLowerCase() === address?.toLowerCase() && (
-                <>
-                  <div className="flex items-center gap-x-4">
-                    {profileIndex.data ? (
-                      <img
-                        src={`/images/wojack/${profileIndex.data}.png`}
-                        alt="pfp"
-                        className="h-[10vh] rounded-full border-2 bg-background p-1 aspect-square object-cover"
-                      />
-                    ) : (
-                      <img
-                        src="/images/profile-pepe.png"
-                        alt="pfp"
-                        className="h-[10vh]"
-                      />
-                    )}
-                    <div className="flex flex-col items-center gap-y-2">
-                      <h1 className="mobile:w-1/2 mobile:self-start mobile:truncate">
-                        <UsernameWrapper>{address}</UsernameWrapper>
-                      </h1>
-                      <div className="flex gap-x-4">
-                        <button
-                          className="bg-yellow-600 w-max px-3 relative z-1 shadow-[4px_4px_2px_#001100bb] mobile:self-start"
-                          onClick={() => modal.show(<CustomizeProfileModal />)}
-                        >
-                          Customize Profile
-                        </button>
-                        <button
-                          className="bg-[#008000] w-max px-3 relative z-1 shadow-[4px_4px_2px_#001100bb] mobile:self-start"
-                          onClick={() => modal.show(<GetUsernameModal />)}
-                        >
-                          Get Username
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="widescreen:absolute widescreen:right-1/4 widescreen:translate-x-full mobile:self-end">
-                    <button
-                      className="bg-red-700 px-4 py-1 shadow-[4px_4px_2px_#001100bb] mobile:text-sm"
-                      onClick={() => disconnect()}
-                    >
-                      Disconnect
-                    </button>
-                  </div>
-                </>
+              {profileIndex.data ? (
+                <img
+                  src={`/images/wojack/${profileIndex.data}.png`}
+                  alt="pfp"
+                  className="h-[10vh] rounded-full border-2 bg-background p-1 aspect-square object-cover"
+                />
+              ) : (
+                <img
+                  src="/images/profile-pepe.png"
+                  alt="pfp"
+                  className="h-[10vh]"
+                />
               )}
-              {username?.data?.toLowerCase() !== address?.toLowerCase() && (
-                <div className="flex items-center gap-x-4">
-                  <img
-                    src="/images/pepe-happy.png"
-                    alt="pfp"
-                    className="h-[10vh]"
-                  />
-                  <div className="flex flex-col items-center gap-y-2">
-                    <h1 className="mobile:w-1/2 mobile:self-start mobile:truncate">
-                      Username:{" "}
-                      <span className="font-bold">
-                        <UsernameWrapper>{address}</UsernameWrapper>
-                      </span>
-                    </h1>
+              <div className="flex items-center gap-x-4">
+                <div className="flex flex-col items-center gap-y-2">
+                  <h1 className="mobile:w-1/2 mobile:self-start mobile:truncate flex gap-x-1">
+                    {username?.data?.toLowerCase() !==
+                      address?.toLowerCase() && <span>Usernme:</span>}
+                    <span className="font-bold">
+                      <UsernameWrapper>{address}</UsernameWrapper>
+                    </span>
+                  </h1>
+                  <div className="flex gap-x-4">
                     <button
-                      className="bg-red-700 px-4 py-1 shadow-[4px_4px_2px_#001100bb] mobile:text-sm"
-                      onClick={() => disconnect()}
+                      className="bg-yellow-600 w-max px-3 relative z-1 shadow-[4px_4px_2px_#001100bb] mobile:self-start"
+                      onClick={() => modal.show(<CustomizeProfileModal />)}
                     >
-                      Disconnect
+                      Customize Profile
                     </button>
+                    {username?.data?.toLowerCase() ===
+                      address?.toLowerCase() && (
+                      <button
+                        className="bg-[#008000] w-max px-3 relative z-1 shadow-[4px_4px_2px_#001100bb] mobile:self-start"
+                        onClick={() => modal.show(<GetUsernameModal />)}
+                      >
+                        Get Username
+                      </button>
+                    )}
                   </div>
                 </div>
-              )}
+              </div>
+              <div className="widescreen:absolute widescreen:right-1/4 widescreen:translate-x-full mobile:self-end">
+                <button
+                  className="bg-red-700 px-4 py-1 shadow-[4px_4px_2px_#001100bb] mobile:text-sm"
+                  onClick={() => disconnect()}
+                >
+                  Disconnect
+                </button>
+              </div>
             </div>
           </div>
         </div>
